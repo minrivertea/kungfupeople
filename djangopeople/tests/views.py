07fbc1f26e8f0497aef5a7478998fcba17a0f2ad
Peter Bengtsson <peter@fry-it.com>
2009-05-10 22:31:59 +0100

    starting to add tests

---
+++ djangopeople/tests/views.py
@@ -0,0 +1,13 @@
+"""
+testing views
+"""
+
+from django.test import TestCase
+from models import KungfuPerson
+
+import views
+
+class SimpleTest(TestCase):
+    def test_test(self):
+        """x
+        """
