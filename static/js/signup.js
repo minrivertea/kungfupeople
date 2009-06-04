8d3ca3ea725337714014159a2c72dbfb7f445a34
Peter Bengtsson <peter@fry-it.com>
2009-06-04 17:36:49 +0100

    draggable signup instead

--- static/js/signup.js
+++ static/js/signup.js
@@ -11,6 +11,13 @@ google.load('maps', '3');
 var INITIAL_LAT = 43.834526782236814;
 var INITIAL_LON = -37.265625;
 
+function __zoom_in() {
+   gmap.setZoom(gmap.getZoom()+1);
+}
+function __zoom_out() {
+   gmap.setZoom(gmap.getZoom()-1);
+}
+
 function reverseGeocode() {
     var lon = $('#id_longitude').val();
     var lat = $('#id_latitude').val();
@@ -22,8 +29,6 @@ function reverseGeocode() {
     }
     var url = 'http://ws.geonames.org/findNearbyPlaceNameJSON?'
     url += 'lat=' + lat + '&lng=' + lon + '&callback=?';
-   $('.text', '#loading-inner:hidden').text('Fetching name by location...');
-   $('#loading-inner:hidden').show();
     jQuery.getJSON(url, function(json) {
         if (typeof json.geonames != 'undefined' && json.geonames.length > 0) {
             // We got results
@@ -38,7 +43,24 @@ function reverseGeocode() {
             if ($('#id_location_description').val() != name) {
                 $('#id_location_description').val(name);
                 $('#id_location_description').parent().yellowFade();
+	       
+	       if (marker) {
+		  
+		  var html = name+"<br>"+countryName+"<br><br><font size=\"-1\">zoom ";
+		  if (gmap.getZoom() < 14)
+		    html += "<a href=\"#\" onclick=\"__zoom_in();return false\">in</a> ";
+		  if (gmap.getZoom() > 2)
+		    html += "<a href=\"#\" onclick=\"__zoom_out();return false\">out</a>";
+		  html += "</font>"
+		  marker.openInfoWindowHtml(html);
+		  
+		  if (gmap.getZoom() >= 3)
+		    gmap.setCenter(marker.getLatLng(), gmap.getZoom()+1);
+		  
+	       }
+	       
             }
+	   
             $('#id_country').val(iso_code).change();
             // Update region field, if necessary
             if (hasRegions(countryName) && place.adminCode1) {
@@ -47,7 +69,7 @@ function reverseGeocode() {
                 $('#id_region').val('');
             }
         }
-       $('#loading-inner:visible').hide();
+       
     });
 }
 
@@ -55,8 +77,37 @@ function hasRegions(country_name) {
     return $('select#id_region optgroup[label="' + country_name + '"]').length;
 }
 
+function __create_marker(point) {
+   marker = new GMarker(point, {draggable:true});
+   
+   GEvent.addListener(marker, "dragstart", function() {
+      gmap.closeInfoWindow();
+   });
+   
+   GEvent.addListener(marker, "dragend", function() {
+      
+      point = this.getLatLng();
+      
+      $('#id_latitude').val(point.lat());
+      $('#id_longitude').val(point.lng());
+      
+      marker.openInfoWindowHtml('<img src="/static/img/loading.gif" width="16" height="16" alt="Please wait..." /> '+
+				+"Please wait...<br/>Fetching location name");
+      if (lookupTimer)
+	clearTimeout(lookupTimer);
+      lookupTimer = setTimeout(reverseGeocode, 1000);
+   });
 
+   gmap.addOverlay(marker);
+   
+}
+
+var zoom_level;
+var point;
+var marker;
 var gmap;
+var lookupTimer = false;
+
 google.setOnLoadCallback(function() {
 
     // Set up the select country thing to show flags    
@@ -90,8 +141,9 @@ google.setOnLoadCallback(function() {
     gmap = new google.maps.Map2(document.getElementById('gmap'));
     gmap.addControl(new google.maps.LargeMapControl3D());
     gmap.addControl(new google.maps.MapTypeControl());    
-    var lookupTimer = false;
     
+    
+   /*
     google.maps.Event.addListener(gmap, "move", function() {
         window.center = gmap.getCenter();
         if (lookupTimer) {
@@ -101,29 +153,46 @@ google.setOnLoadCallback(function() {
         $('#id_latitude').val(center.lat());
         $('#id_longitude').val(center.lng());
     });
+    */
+   
+   /*
     google.maps.Event.addDomListener(document.getElementById('crosshair'),
         'dblclick', function() {
             gmap.zoomIn();
         }
     );
+    */
     
     /* The first time the map is hovered, scroll the page */
-    $('#gmap').one('click', function() {
-        $('html,body').animate({scrollTop: $('#gmap').offset().top}, 500);
+   //GEvent.addListener(gmap,"click", function(overlay, latlng) {
+   //   point = latlng;
+   //});
+		      
+    $('#gmap').one('click', function(event) {
+       $('html,body').animate({scrollTop: $('#gmap').offset().top}, 500);
+       if (!marker) {
+	  if (!point)
+	    point = gmap.getCenter();
+	  __create_marker(point);
+	  marker.openInfoWindowHtml("Drag the marker to your location on the map");
+       }
     });
-    
-    var point;
-    /* If latitude and longitude are populated, center there */
+   
+    // If latitude and longitude are populated, center there 
     if ($('#id_latitude').val() && $('#id_longitude').val()) {
         point = new google.maps.LatLng(
             $('#id_latitude').val(),
             $('#id_longitude').val()
         );
-        gmap.setCenter(point, 9);
+       zoom_level = 7;
+       __create_marker(point);
+       
     } else {
-        gmap.setCenter(new google.maps.LatLng(INITIAL_LAT, INITIAL_LON), 3);
+       point = new google.maps.LatLng(INITIAL_LAT, INITIAL_LON);
+       zoom_level = 3;
     }
    
+   gmap.setCenter(point, zoom_level);
    
    // When you fill in the club_url, prefill the club name if possible
    $('#id_club_url').change(function() {
@@ -151,8 +220,5 @@ google.setOnLoadCallback(function() {
                   );
       
    });
-   
-   
-   $('#loading-inner').hide();
    
 });
