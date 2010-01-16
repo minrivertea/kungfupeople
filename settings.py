a33faee0f3c2d09190917faa8869722c65d3699e
Peter Bengtsson <peter@fry-it.com>
2010-01-16 18:54:31 +0000

    moved context processors

--- settings.py
+++ settings.py
@@ -79,7 +79,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
-    'context_processors.context',
+    'djangopeople.context_processors.context',
 )
 
 
